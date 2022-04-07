d864bdf9fc2abeb3d1b666b23e3b65dc292f3046
Blacked1234 <72291767+Blacked1234@users.noreply.github.com>
2022-04-07 15:30:44 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/oldcooky/SplashScreen.kt
@@ -0,0 +1,32 @@
+package com.example.oldcooky
+
+import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.WindowManager
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        window.apply {
+            clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            statusBarColor = Color.TRANSPARENT
+        }
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashScreen, MainActivity::class.java)
+            startActivity(intent)
+        }, 3000)
+    }
+
+}
